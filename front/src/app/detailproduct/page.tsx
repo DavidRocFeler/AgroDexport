523d086b401943538a11378131ada0c1f1ab4d02
dvdvalencia <dvdvalenciavasquez88@gmail.com>
2024-10-16 13:23:11 -0500

    Guardando cambios locales en page.tsx

--- front/src/app/detailproduct/page.tsx
+++ front/src/app/detailproduct/page.tsx
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-// import AllDetailView from '@/views/AllDetailView'
-// import React from 'react'
 
-// const ProductDetail = () => {
-//   return (
-//     <div>
-//       <AllDetailView/>
-//     </div>
-//   )
-// }
-
-// export default ProductDetail
-=======
 import AllDetailView from '@/views/AllDetailView'
 import React from 'react'
 
@@ -24,4 +11,3 @@ const ProductDetail = () => {
 }
 
 export default ProductDetail
->>>>>>> 0e60c142d1706875b233d0532891a0bbce90b424
