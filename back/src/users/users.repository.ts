6e5a901cc45175b43dea31b466a8083b2036433c
namemc95 <moralesdreiser5@gmail.com>
2024-10-15 19:56:06 -0500

    saving beforer pull request

--- back/src/users/users.repository.ts
+++ back/src/users/users.repository.ts
@@ -2,37 +2,61 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User, Credential } from '@prisma/client';
 import { validateExists } from '../helpers/validation.helper';
+import { CreateUserDto } from './createUser.dto';
+import { User } from '@clerk/nextjs/dist/types/server';
 
 @Injectable()
 export class UsersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createUser(user: Omit<User, 'user_id' | 'credential_id'>, email: string, password: string): Promise<Omit<User, 'credential_id'>> {
-    const checkEmail = await this.findCredentialByEmail(email);
-    validateExists(checkEmail, 'exists', 'Email already exists');
+  // async createUser(user: Omit<User, 'user_id' | 'credential_id'>, email: string, password: string): Promise<Omit<User, 'credential_id'>> {
+    // const checkEmail = await this.findCredentialByEmail(email);
+    // validateExists(checkEmail, 'exists', 'Email already exists');
 
-    if (user.nDni) {
-      const checkDni = await this.prisma.user.findUnique({
-        where: { nDni: user.nDni },
-      });
-      validateExists(checkDni, 'exists', 'DNI already exists');
+    // if (user.nDni) {
+      // const checkDni = await this.prisma.user.findUnique({
+        // where: { nDni: user.nDni },
+      // });
+      // validateExists(checkDni, 'exists', 'DNI already exists');
+    // }
+
+    // const newCredential = await this.prisma.credential.create({
+      // data: {
+        // email: email,
+        // password: password,
+      // },
+    // });
+
+    // const newUser = await this.prisma.user.create({
+      // data: {
+        // ...user,
+        // credential_id: newCredential.credential_id, 
+      // },
+    // });
+
+    // return newUser;
+  // }
+
+  async creareUser (userData: CreateUserDto ): Promise<User> {
+
+    const {user_name, user_lastname, email, password, confirmPassword, nDni, birthday, phone, country, role_id} = userData
+    const user = await this.findCredentialByEmail(email)
+
+    if (!user) {
+      const newUser = await this.prisma.user.create({
+        data : {
+          user_name: user_name,
+          user_lastname: user_lastname,
+          email: email,
+          password: password,
+          nDni: nDni,
+          birthday: birthday,
+          phone: phone,
+          country: country,
+          role_id: role_id
+        }
+      })
     }
-
-    const newCredential = await this.prisma.credential.create({
-      data: {
-        email: email,
-        password: password,
-      },
-    });
-
-    const newUser = await this.prisma.user.create({
-      data: {
-        ...user,
-        credential_id: newCredential.credential_id, 
-      },
-    });
-
-    return newUser;
   }
 
   async findCredentialByEmail(email: string): Promise<Credential | null> {
@@ -40,6 +64,7 @@ export class UsersRepository {
       where: { email },
     });
   }
+
 
   async findUserByCredentialId(credential_id: string): Promise<User | null> {
     return this.prisma.user.findUnique({
