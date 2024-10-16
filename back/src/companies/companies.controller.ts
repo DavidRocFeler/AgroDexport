56b9616ab2c9eafff0dfcc19b1c233612403da13
jatapsilver <95506425+jatapsilver@users.noreply.github.com>
2024-10-16 15:04:18 -0500

    Mis cambios locales

--- back/src/companies/companies.controller.ts
+++ back/src/companies/companies.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { CompanyService } from './companies.service';
+import { CreateCompanyDto } from './createCompany.dto';
 
 @Controller('companies')
-export class CompaniesController {}
+export class CompanyController {
+
+    constructor(private readonly companyServices:CompanyService){}
+
+    @Get()
+    async getAllCompanies() {
+    const companies = await this.companyServices.getAllCompaniesServices();
+    return companies;
+    }
+
+    @Get(':id') 
+    async getCompanyById(@Param('id') companyId: string) {
+    const company = await this.companyServices.getCompanyByIdServices(companyId);
+    return company;
+    }
+
+    @Post()
+    async createCompany(@Body() companyData: CreateCompanyDto) {
+    const newCompany = await this.companyServices.createCompanyServices(companyData);
+    return {
+        message: 'Company created successfully',
+        data: newCompany,
+      };
+    }
+
+    @Put(':id') // Este endpoint actualizará la compañía específica por ID
+    async updateCompany(
+    @Param('id') companyId: string,
+    @Body() companyData: CreateCompanyDto,
+    ) {
+    const updatedCompany = await this.companyServices.updateCompanyServices(companyId, companyData);
+    return updatedCompany;
+    }
+
+}
