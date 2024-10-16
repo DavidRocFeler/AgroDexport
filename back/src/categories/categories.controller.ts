d2eeb0441eb90726234f2a57f1fe5c98ec976e22
davidrocfeler <rdpalominop@ucvvirtual.edu.pe>
2024-10-15 21:17:14 -0500

    now is the new complete branch main, project === 10%

--- back/src/categories/categories.controller.ts
+++ back/src/categories/categories.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from "@nestjs/common";
+import { CategoriesService } from "./categories.service";
+import { CreateCategoryDto } from "./createCategory.dto";
+import { ApiTags } from "@nestjs/swagger";
 
-@Controller('categories')
-export class CategoriesController {}
+@ApiTags("Categories")
+@Controller("categories")
+export class CategoriesController {
+    constructor(private readonly categoriesService: CategoriesService) {}
+
+    @HttpCode(200)
+    @Get()
+    async getCategories() {
+        return this.categoriesService.getCategoriesService();
+    }
+
+    @Post("sedeer")
+    async preloadCategories(@Body() categoryData: CreateCategoryDto){
+        return this.categoriesService.preloadCategoriesService();
+    }
+}
