371638abb85fdf1a77a64180a2e6b1b2efd0e305
marianapere <marianamirpereyra@gmail.com>
2024-10-17 17:57:33 -0300

    ADD vista HelpView con chatbot en la ruta /help

---
+++ front/src/app/help/page.tsx
@@ -0,0 +1,11 @@
+import HelpView from "@/views/HelpView";
+import React from "react";
+
+const Help: React.FC = () => {
+  return (
+    <>
+      <HelpView />
+    </>
+  );
+};
+export default Help;
