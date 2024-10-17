4987d027f29d053f4e6612ecd4104de994b45e13
jatapsilver <95506425+jatapsilver@users.noreply.github.com>
2024-10-17 02:00:28 -0500

    Stop tracking dist/ directory and update .gitignore

--- back/dist/users/users.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dtos/createUser.dto';
-export declare class UsersController {
-    private readonly userServices;
-    constructor(userServices: UsersService);
-    updateUser(id: string, userData: CreateUserDto): Promise<void>;
-}
--- back/dist/users/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { CreateUserDto } from './dtos/createUser.dto';
-import { UsersRepository } from './users.repository';
-export declare class UsersService {
-    private readonly userRepository;
-    constructor(userRepository: UsersRepository);
-    updateUser(id: string, userData: CreateUserDto): Promise<void>;
-}
