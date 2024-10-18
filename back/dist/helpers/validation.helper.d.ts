39586447c2b8435ffe36c959d5b79c2d9e16a3ff
dvdvalencia <dvdvalenciavasquez88@gmail.com>
2024-10-18 16:55:21 -0500

    Dejar de rastrear mi_carpeta

--- back/dist/helpers/validation.helper.d.ts
+++
@@ -1,4 +0,0 @@
-type ValidationType = 'exists' | 'notExists';
-export declare function validateExists(value: any, type: ValidationType, message: string): void;
-export declare function validateRequestBodyNotEmpty(updateData: any): void;
-export {};
