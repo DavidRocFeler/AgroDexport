820566c7e72ebc28a780bc35e9d62cbfb124c6eb
dvdvalencia <dvdvalenciavasquez88@gmail.com>
2024-10-18 15:30:16 -0500

    back

--- front/src/views/MarketView.tsx
+++ front/src/views/MarketView.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ProductSearch from "@/components/ProductSearch"
 import Image from 'next/image'
 import ProductCard from '@/components/ProductCard';
-import PaypalView from '@/components/PaypalView';
+
 
 const MarketView: React.FC = () => {
   return (
     <div style={{background: "white"}}>
       <Image src="/ImgMarketView.png" alt="Market View" layout="responsive" width={500} height={300} />
       <ProductSearch />
-      {/* <PaypalView /> */}
+    
       <ProductCard />
     </div>
   )
