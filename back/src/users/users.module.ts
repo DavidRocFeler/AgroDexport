6032086b34d38f3b7e7464902d86e0d788ea1417
jorge <jorgemartinez.jam@gmail.com>
2024-10-19 20:28:08 -0300

    Corrección de error en notificaciones

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
-import { PrismaService } from '../prisma/prisma.service'; // Importar el servicio Prisma
-import { NotificationsModule } from 'src/notifications/notifications.module';
+// import { PrismaService } from '../prisma/prisma.service'; // Importar el servicio Prisma
+// import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
+  // imports: [NotificationsModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
-  exports: [UsersRepository],
-  imports: [NotificationsModule]
+  providers: [UsersService, UsersRepository],
+  exports: [UsersRepository]
 })
 export class UsersModule {}
