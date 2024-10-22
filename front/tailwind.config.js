e9977806f7266ad7052bb2a3df9e173d5a232850
marianapere <marianamirpereyra@gmail.com>
2024-10-22 12:31:18 -0300

    Actualizando la configuración de Tailwind CSS

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,9 +1,12 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Escanea archivos de React
+    "./public/index.html", // Escanea archivo HTML
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
