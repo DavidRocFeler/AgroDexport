8dce5345b25bfdf22afa21848f898f13dddfe4ef
marianapere <marianamirpereyra@gmail.com>
2024-10-22 13:07:39 -0300

    Guardando cambios en ProfileView2.tsx

--- front/src/views/ProfileView2.tsx
+++ front/src/views/ProfileView2.tsx
@@ -107,29 +107,28 @@ const ProfileView2: React.FC = () => {
             <div className="p-4 w-1/2">
               <div className="h-full bg-gray-100 p-8 rounded">
                 <div className="flex w-screen h-screen dark:bg-gray-900 justify-center items-center">
+                  {/*  */}
                   <div className="w-[150px] text-gray-900 dark:text-gray-100">
                     <div className="relative w-full group">
                       <label className="text-xs text-gray-400">
-                        Select Category
+                        Select Company
                       </label>
                       <button className="py-2.5 px-3 w-full md:text-sm text-site bg-transparent border border-dimmed  focus:border-brand focus:outline-none focus:ring-0 peer flex items-center justify-between rounded font-semibold">
-                        All
+                        Company1
                       </button>
                       <div className="absolute z-[99] top-[100%] left-[50%] translate-x-[-50%] rounded-md overflow-hidden shadow-lg min-w-[200px] w-max peer-focus:visible peer-focus:opacity-100 opacity-0 invisible duration-200 p-1 bg-gray-100 dark:bg-gray-800  border border-dimmed text-xs md:text-sm">
                         <div className=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                          All (9)
+                          Company1
                         </div>
                         <div className=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                          Full Stack (6)
+                          Company2
                         </div>
                         <div className=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                          Front End (1)
+                          Company3
                         </div>
+
                         <div className=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                          Freelance (1)
-                        </div>
-                        <div className=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                          New Stack Project (1)
+                          Company4
                         </div>
                       </div>
                     </div>
@@ -138,11 +137,7 @@ const ProfileView2: React.FC = () => {
               </div>
             </div>
             <div className="p-4 w-1/2">
-              <div className="h-full bg-gray-100 p-8 rounded">
-                <p className="leading-relaxed mb-6">
-                  Fusce pharetra eget augue ac condimentum.
-                </p>
-              </div>
+              <div className="h-full bg-gray-100 p-8 rounded"></div>
             </div>
           </div>
         </div>
