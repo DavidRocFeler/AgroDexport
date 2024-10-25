a39ee9efccf03c67afbce45cf96d797817e2bd6e
marianapere <marianamirpereyra@gmail.com>
2024-10-25 11:41:38 -0300

    Add/ Interface IPublishProductProps,component FormPublishProduct, view PublishProductView, /publishproducts

---
+++ front/src/views/PublishProductView.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import FormPublishProduct from "../components/FormPublishProduct";
+
+const PublishProductView: React.FC = () => {
+  return (
+    <div className="container mx-auto py-8 font-inter">
+      <h1 className="text-[48px] md:text-[96px] text-center mb-12 font-inter">
+        Publish Products
+      </h1>
+      <div className="border border-black max-w-3xl mx-auto p-8 "></div>
+    </div>
+  );
+};
+
+export default PublishProductView;
