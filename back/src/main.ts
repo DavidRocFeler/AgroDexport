e31b19ca4750eda22325b13a68965fb48dbe9682
jorge <jorgemartinez.jam@gmail.com>
2024-10-26 12:32:30 -0300

    Pruebas con cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   console.log('CORS origin:', process.env.DOMAIN_FRONT);
  
   app.enableCors({
-    origin: process.env.DOMAIN_FRONT,  
+    origin: 'https://agrodexports.onrender.com',  
     methods: 'GET,POST,PUT,DELETE',    
     credentials: true,                 // Permitir cookies o autenticación
     allowedHeaders: ['Content-Type', 'Authorization']
