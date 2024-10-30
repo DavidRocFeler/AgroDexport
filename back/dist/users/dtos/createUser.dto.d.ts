58d5580b4d2f27bd2cf8092f90a1a8d245d33d4d
jatapsilver <95506425+jatapsilver@users.noreply.github.com>
2024-10-29 20:46:20 -0500

    Stop tracking dist folder

--- back/dist/users/dtos/createUser.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateUserDto {
-    user_name: string;
-    user_lastname: string;
-    email: string;
-    password: string;
-    confirm_password: string;
-    isOlder: boolean;
-    role_name: string;
-}
