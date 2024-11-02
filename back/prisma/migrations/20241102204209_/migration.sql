aef1a84cc1ba8f891d44f82f6d05234938e659ab
jorge <jorgemartinez.jam@gmail.com>
2024-11-02 17:58:07 -0300

    Cambios minimos en el marketview y users

---
+++ back/prisma/migrations/20241102204209_/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "companies" ALTER COLUMN "account_paypal" SET DEFAULT 'ATUWOIjpu_gq_k6k28V2zGm2DRCGdv_5PfrJ_J1H38xze2IGLtMXhDMUJWLqsYwAqSxfJkyzqODmKBCx',
+ALTER COLUMN "company_logo" SET DEFAULT 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYc3Fkk0tLl5MMO0rUaiO5qUfdLzbaPhb07g&s';
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "profile_picture" SET DEFAULT 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYc3Fkk0tLl5MMO0rUaiO5qUfdLzbaPhb07g&s';
