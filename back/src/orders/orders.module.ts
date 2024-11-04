099eb61c17135ec4d63a009296560355acf9924c
jorge <jorgemartinez.jam@gmail.com>
2024-11-04 18:50:20 -0300

    Nuevas notificaciones e emails

--- back/src/orders/orders.module.ts
+++ back/src/orders/orders.module.ts
@@ -3,14 +3,15 @@ import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { OrderRepository } from './orders.repositiry';
 import { CompaniesModule } from 'src/companies/companies.module';
-import { UsersModule } from 'src/users/users.module';
-import { CategoriesModule } from 'src/categories/categories.module';
-import { NotificationsModule } from 'src/notifications/notifications.module';
-import { CompanyProductsRepository } from 'src/company-products/company-products.repository';
-import { AddressesRepository } from 'src/addresses/adresses.repository';
+import { UsersModule } from '../users/users.module';
+import { CategoriesModule } from '../categories/categories.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { CompanyProductsRepository } from '../company-products/company-products.repository';
+import { AddressesRepository } from '../addresses/adresses.repository';
+import { EmailModule } from '../nodemail/nodemail.module';
 
 @Module({
-  imports: [CompaniesModule, UsersModule, CategoriesModule, NotificationsModule],
+  imports: [CompaniesModule, UsersModule, CategoriesModule, NotificationsModule, EmailModule],
   controllers: [OrdersController],
   providers: [OrdersService, OrderRepository, CompanyProductsRepository, AddressesRepository],
   exports: [OrderRepository, OrdersService]
